47349a90259caba34e96bf3ed5a4af5eed6861f2
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2024-06-28 12:17:03 +0545

    day4: routing setup, block content, template inheritance

--- djangoToDo/tasksTodo/models.py
+++ djangoToDo/tasksTodo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title= models.CharField(max_length=200)
+    description= models.TextField(null=True, blank=True)
+    due_date= models.DateTimeField()
+    due_time= models.TimeField()
+    is_completed= models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} - {self.due_date} - {self.due_time} - {self.is_completed}'
+
+
